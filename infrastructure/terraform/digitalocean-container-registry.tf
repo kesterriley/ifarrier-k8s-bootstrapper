5ce4b96d8d87338ae3839a431332c816adee3cc5
Kester Riley <kesterriley@hotmail.com>
2025-03-11 13:56:18 +0000

    Renamed deployments and pods.

--- infrastructure/terraform/digitalocean-container-registry.tf
+++ infrastructure/terraform/digitalocean-container-registry.tf
@@ -2,6 +2,6 @@
 resource "digitalocean_container_registry" "registry" {
   count                  = var.enable_container_registry ? 1 : 0
   name                   = var.container_registry_name
-  subscription_tier_slug = "starter"
+  subscription_tier_slug = "basic"
   region                 = "ams3"
 }
